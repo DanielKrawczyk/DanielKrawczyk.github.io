fdbb3e57bc74d8435274091f96be1d4dce8d3f4e
DanielKrawczyk <80767727+DanielKrawczyk@users.noreply.github.com>
2021-03-22 15:09:40 +0100

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,58 +1,80 @@
-(function addEventListeners(){
+(() => {
+
     let isActive = false;
-    document.getElementById('more-button').addEventListener('click', ()=>{
+    document.getElementById('more-button').addEventListener('click', () => {
         const doc = document.querySelector('.text-more_txt');
-        if (isActive === false){
+        if (isActive === false) {
             doc.classList.add('active');
-            setTimeout(()=>{
+
+            setTimeout(() => {
                 doc.classList.add('show');
             }, 100);
+
             isActive = true;
             document.getElementById('more-button').innerHTML = 'Pokaż mniej';
-        }else{
+        } else {
             doc.classList.remove('show');
-            setTimeout(()=>{
+
+            setTimeout(() => {
                 doc.classList.remove('active');
             }, 600);
+
             isActive = false;
             document.getElementById('more-button').innerHTML = 'Pokaż więcej';
         };
     });
+
     let itemActive = false;
     document.querySelectorAll('.list-item').forEach(el => el.addEventListener('click', ()=>{
-        if(itemActive === false){
+        if (itemActive === false) {
             el.classList.add('active');
-            setTimeout(()=>{
+
+            setTimeout(() => {
                 el.classList.add('show');
             }, 100);
+
             itemActive = true;
-        }else{
+        } else {
             el.classList.remove('show');
-            setTimeout(()=>{
+
+            setTimeout(() => {
                 el.classList.remove('active');
             }, 600);
+
             itemActive = false;
         };
     }));
+
     let navActive = false;
     const navCont = document.getElementById('nav-content');
-    document.getElementById('nav-button').addEventListener('click', ()=>{
-        if(navActive === false){
-            document.getElementById('nav-button').classList.add('open');
+    const navButton = document.getElementById('nav-button');
+    document.getElementById('nav-button').addEventListener('click', () => {
+        if (navActive === false) {
+            navButton.classList.add('open');
             navCont.classList.add('active');
             navActive = true;
-        }else{
-            document.getElementById('nav-button').classList.remove('open');
+        } else {
+            navButton.classList.remove('open');
             navCont.classList.remove('active');
             navActive = false;
         }
     });
+
+    document.querySelectorAll('.nav-bar_item').forEach(el => el.addEventListener('click', () => {
+        if (navActive === true) {
+            navButton.classList.remove('open');
+            navCont.classList.remove('active');
+            navActive = false;
+        }
+    }));
 })();
-window.onscroll = ()=>onScroll();
-function onScroll(){
+window.onscroll = () => onScroll();
+
+function onScroll() {
     const navbar = document.getElementById('nav');
     const top = navbar.offsetTop;
-    if(window.pageYOffset > top){
+
+    if (window.pageYOffset > top) {
         navbar.classList.add('active');
     } else {
         navbar.classList.remove('active');
