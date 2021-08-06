4ee7c3faf519a150614f1e11ccebf7fb79fcd891
DanielKrawczyk <80767727+DanielKrawczyk@users.noreply.github.com>
2021-08-06 18:40:04 +0200

    typing animation fixed

--- anims/anims.js
+++ anims/anims.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const scroll = document.querySelectorAll('.scroll');
 
 function elIn(el, x = 1) {
@@ -36,4 +38,32 @@ function onScrollEvent() {
 
 window.addEventListener('scroll', () => {
     onScrollEvent();
-})
+});
+
+function stringToArray(str) {
+    let arr = new Array();
+    for (let i = 0; i < str.length; i++) {
+        arr.push(str[i]);
+    }
+    return arr;
+};
+
+const arr = stringToArray("const website = new Coffee();");
+let index = 0;
+let content = "";
+const time = 125;
+
+function typing() {
+    content += arr[index];
+    if (arr[index] !== "<") document.getElementById('typing').innerHTML = content + "<span>|</span>";
+    index++;
+    if (index < arr.length) {
+        setTimeout(() => {
+            typing();
+        }, time);
+    }
+};
+
+setTimeout(() => {
+    typing();
+}, 1000);
